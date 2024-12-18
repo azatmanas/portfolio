81142ddd62b5541bf70040ac775c89526d7488df
AZAT MANAS <ZTMANAS@GMAIL.COM>
2024-12-17 19:48:18 -0500

    projects are updated

---
+++ components/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header>
+      <a href="#" className="logo">
+        <span>azat</span>manas
+      </a>
+      <nav className="nav__bar"></nav>
+    </header>
+  );
+}

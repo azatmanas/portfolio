a862d9f5c24176747ba4d2309039ce60d55bdc82
AZAT MANAS <ZTMANAS@GMAIL.COM>
2025-07-29 00:58:54 -0400

    Header jsx updated

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useEffect, useRef, useState } from "react";
 
 const SOCIAL_LINKS = [
   {
@@ -28,9 +29,35 @@ const NAV_ITEMS = [
 ];
 
 const Header = () => {
+  const [menuActive, setMenuActive] = useState(false);
+  const headerRef = useRef();
+  const menuRef = useRef();
+
+  const toggleMenu = () => {
+    setMenuActive((prev) => !prev);
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setMenuActive(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
-    <header className="flex justify-between items-center p-4 bg-gray-900 text-white">
+    <header
+      ref={headerRef}
+      className={`${
+        menuActive ? "active" : ""
+      } flex justify-between items-center p-4 bg-gray-900 text-white`}
+    >
       {/* Logo/Brand */}
+      <div
+        id="menu-bars"
+        className={`fas fa-bars ${menuActive ? "fa-times" : ""}`}
+        onClick={toggleMenu}
+        ref={menuRef}
+      ></div>
       <div className="logo">
         <a href="#" className="text-2xl font-bold">
           <span className="text-blue-400">azat</span> manas
